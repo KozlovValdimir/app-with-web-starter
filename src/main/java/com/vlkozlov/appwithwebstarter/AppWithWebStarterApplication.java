efd7c7ba60fa6de8335f84e208ca5461c07b5267
Vladimir Kozlov <vlkozlov@neoflex.ru>
2021-11-11 23:54:20 +0300

    initial commit Vladimir Kozlov

---
+++ src/main/java/com/vlkozlov/appwithwebstarter/AppWithWebStarterApplication.java
@@ -0,0 +1,13 @@
+package com.vlkozlov.appwithwebstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWithWebStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWithWebStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vlkozlov/appwithwebstarter/AppWithWebStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vlkozlov.appwithwebstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWithWebStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
